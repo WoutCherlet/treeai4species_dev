f77dfa64726b8f8600fb5cd130e49efbb2d5815b
Wout Cherlet <Wout.Cherlet@Ugent.be>
2025-06-16 18:45:02 +0200

    got model running finally but still some data format issue, TODO

--- mmdetection_cfgs/cascade_rcnn_hrnetv2p_w18.py
+++ mmdetection_cfgs/cascade_rcnn_hrnetv2p_w18.py
@@ -1,21 +1,25 @@
-_base_ = [
-    'mmdet::_base_/models/cascade-rcnn_hrnetv2p-w18.py',
-    'mmdet::_base_/datasets/coco_detection.py',
-    'mmdet::_base_/schedules/schedule_1x.py',
-    'mmdet::_base_/default_runtime.py'
-]
+# _base_ = [
+#     'mmdet::_base_/models/cascade-rcnn_hrnetv2p-w18.py',
+#     'mmdet::_base_/datasets/coco_detection.py',
+#     'mmdet::_base_/schedules/schedule_1x.py',
+#     'mmdet::_base_/default_runtime.py'
+# ]
+_base_ = "mmdet::hrnet/cascade-rcnn_hrnetv2p-w18-20e_coco.py"
+
 
 # ===================== MODEL =====================
 model = dict(
-    backbone=dict(frozen_stages=4),  # Freeze full HRNet backbone
     roi_head=dict(
         bbox_head=[
-            dict(num_classes=53),
-            dict(num_classes=53),
-            dict(num_classes=53),
+            dict(type='Shared2FCBBoxHead', num_classes=53),
+            dict(type='Shared2FCBBoxHead', num_classes=53),
+            dict(type='Shared2FCBBoxHead',num_classes=53),
         ]
     )
 )
+
+# pretrained weights
+load_from = "https://download.openmmlab.com/mmdetection/v2.0/hrnet/cascade_rcnn_hrnetv2p_w18_20e_coco/cascade_rcnn_hrnetv2p_w18_20e_coco_20200210-434be9d7.pth"
 
 # ===================== DATASET =====================
 dataset_type = 'CocoDataset'
@@ -26,7 +30,7 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', scale=img_scale, keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='RandomFlip', prob=0.5),
     dict(type='PackDetInputs')
 ]
 test_pipeline = [
@@ -70,17 +74,34 @@ test_evaluator = val_evaluator
 # ===================== TRAINING =====================
 train_cfg = dict(max_epochs=36, val_interval=1)
 
+val_cfg = dict(type='ValLoop')  # The validation loop type
+test_cfg = dict(type='TestLoop')  # The testing loop type
+
 # ===================== OPTIMIZATION =====================
 optim_wrapper = dict(
     type='OptimWrapper',
-    optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+    optimizer=dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
 )
 
 # ===================== LOGGING =====================
 default_hooks = dict(
     logger=dict(type='LoggerHook', interval=50),
     checkpoint=dict(type='CheckpointHook', interval=1, save_best='coco/bbox_mAP_50', rule='greater'),
-    param_scheduler=dict(type='MultiStepLR', milestones=[27, 33], gamma=0.1),
 )
 
-visualizer = dict(vis_backends=[dict(type='LocalVisBackend'), dict(type='TensorboardVisBackend')])
+param_scheduler = [
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=36,
+        by_epoch=True,
+        milestones=[27, 33],
+        gamma=0.1
+    )
+]
+
+visualizer = dict(
+    type='Visualizer', 
+    vis_backends=[dict(type='LocalVisBackend'), dict(type='TensorboardVisBackend')],
+    name='visualizer'
+)
