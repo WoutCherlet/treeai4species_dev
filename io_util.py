3bfb8b7fd3a24f8ea1127cb00c5965ec4467ed37
Wout Cherlet <Wout.Cherlet@Ugent.be>
2025-06-16 15:23:44 +0200

    files

--- io_util.py
+++ io_util.py
@@ -1,3 +1,25 @@
 import os
 
-# TODO: util function for io here
+import pandas as pd
+
+
+def label_file_to_list(label_file):
+    # # format: class x_center y_center width height
+    data = pd.read_csv(label_file, sep=" ", header=None)
+    data.columns = ["label", "x_center", "y_center", "width", "height"]
+    return data
+
+def xy_to_img_coordinates(labels, img):
+    height, width, _ = img.shape
+    labels["x_center"] = labels["x_center"]*width
+    labels["width"] = labels["width"]*width
+    labels["y_center"] = labels["y_center"]*height
+    labels["height"] = labels["height"]*height
+    return labels
+
+
+def label_files_to_coco(label_dir):
+
+    # todo: convert label files to coco json file (see openmm website)
+
+    raise NotImplementedError
